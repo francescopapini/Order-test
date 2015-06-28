979d51684ac13c8167a83ea912c0b1cf5f301e96
francescopapini01@gmail.com <francescopapini01@gmail.com>
2015-06-28 20:13:35 +0100

    adding validation for signle order

--- ordertest/app/models/order.rb
+++ ordertest/app/models/order.rb
@@ -9,54 +9,50 @@ class Order < ActiveRecord::Base
     if file.content_type.include? "csv" 
       CSV.foreach(file.path, headers: true) do |row|
         row.to_hash
-        Order.create(order_date: row["Order Date"],
+        @order = self.new(order_date: row["Order Date"],
          customer_id: row["Customer ID"],
          supplier_id: row["Supplier ID"],
          delivery_address: row["Delivery Address"],
-         total_order_value_pence: (row["Total Order Value"].to_f * 100).to_i,
+         total_order_value_pence: row["Total Order Value"].delete(",").delete(".").to_i,
          currency: row["Currency"])
+
+        @order.save if !self.order_already_exists(@order.order_date, @order.customer_id, @order.supplier_id, @order.delivery_address, @order.currency, @order.total_order_value_pence)
       end
 
     elsif file.content_type.include? "plain" 
-
       lines = []
       File.open(file.path).each do |line|
-
        line = line.strip.split(";")
-  
-      
        line[3].gsub!(/"/, '')
        line[4].gsub!(".", '')
        lines << line
      end
-
      rows = {}
      for i in 1..(lines.length - 1) do
       rows[i-1] = {lines[0][0] => lines [i][0], lines[0][1] => lines [i][1], lines[0][2] => lines[i][2], lines[0][3] => lines[i][3], lines[0][4] => lines[i][4], lines[0][5] => lines[i][5]}
     end 
-
     for i in 0..(rows.length - 1) do
       date = rows[i]["Order Date"].split("/")
       if rows[-1] = "USD"
        rows[i]["Order Date"] = Date.new(date[2].to_i, date[0].to_i, date[1].to_i)
-      else
+     else
        rows[i]["Order Date"] = Date.new(date[2].to_i, date[1].to_i, date[0].to_i)
-       end
+     end
 
-
-      @order = Order.create(order_date: rows[i]["Order Date"],
+     self.new(order_date: rows[i]["Order Date"],
        customer_id: rows[i]["Customer ID"],
        supplier_id: rows[i]["Supplier ID"],
        delivery_address: rows[i]["Delivery Address"],
-       total_order_value_pence: (rows[i]["Total Order Value"].to_f * 100).to_i,
+       total_order_value_pence: rows[i]["Total Order Value"].delete(",").delete(".").to_i,
        currency: rows[i]["Currency"])
-    end
-      binding.pry
 
-    else
-    end
+     self.save if !order_already_exists(self.order_date, self.customer_id, self.supplier_id, self.delivery_address, self.currency, self.total_order_value_pence)
 
-  end
+   end
+ else
+ end
+
+end
 
 
 # converts an order from default currency (USD) into any rate for the specified date
@@ -83,7 +79,16 @@ return new_value
 end
 
 def total_order_value
-  total_order_value_pence / 100
+  total_order_value_pence.to_f / 100
+end
+
+def self.order_already_exists(order_date, customer_id, supplier_id, delivery_address, currency, total_order_value_pence)
+  order = self.find_by_order_date_and_customer_id_and_supplier_id_and_delivery_address_and_currency_and_total_order_value_pence(order_date, customer_id, supplier_id, delivery_address, currency, total_order_value_pence)
+  if order.blank?
+    return false
+  else
+    return true
+  end
 end
 
 end
