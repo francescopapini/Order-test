00b49db3963f2622936e47b268bd298415aa4fb8
francescopapini01@gmail.com <francescopapini01@gmail.com>
2015-06-28 21:55:26 +0100

    adding comments

--- ordertest/app/controllers/orders_controller.rb
+++ ordertest/app/controllers/orders_controller.rb
@@ -12,23 +12,23 @@ class OrdersController < ApplicationController
      supplier: { id: @order.supplier_id },
      date: @order.order_date.strftime("%d-%m-%Y"),
      total_order_value: {local_currency_code: @order.currency, local_value: number_to_currency(@order.total_order_value_pence, unit: ""), value: number_to_currency(@order.convert_order_to_historical_rate(@order.total_order_value_pence, "GBP", @order.order_date), unit: "")}
-   } 
-   respond_to do |format|
-    format.html 
-    format.json { render json: JSON.pretty_generate(json_order) }
+    } 
+    respond_to do |format|
+      format.html 
+      format.json { render json: JSON.pretty_generate(json_order) }
+    end
   end
-end
 
-def new
-end
-
-def import
-  if params[:file]
-    Order.import(params[:file])
-    redirect_to root_url, notice: "Orders Imported!"
-  else
-    redirect_to new_order_path, notice: "No File To Import, Please Add File"
+  def new
   end
-end
 
+  def import
+    if params[:file]
+      Order.import(params[:file])
+      redirect_to root_url, notice: "Orders Imported!"
+    else
+      redirect_to new_order_path, notice: "No File To Import, Please Add File"
+    end
+  end
+  
 end
--- ordertest/app/models/order.rb
+++ ordertest/app/models/order.rb
@@ -1,34 +1,43 @@
 class Order < ActiveRecord::Base
 
   validates :order_date, :customer_id, :supplier_id, :delivery_address, :currency, :total_order_value_pence, presence: true
-  validates :customer_id, numericality: {greater_than_or_equal_to: 0}
-  validates :supplier_id, numericality: {greater_than_or_equal_to: 0}
-  validates :total_order_value_pence, numericality: {greater_than_or_equal_to: 0}
-
+  validates :customer_id, :supplier_id, :total_order_value_pence, numericality: {greater_than_or_equal_to: 0}
+  
+  # used when importing .csv and .txt files
   def self.import(file)
-    if file.content_type.include? "csv" 
+   #case 1: importing CSV file
+   if file.content_type.include? "csv" 
+
+      # reads individual rows from the .csv file and creates a hash for each of them 
       CSV.foreach(file.path, headers: true) do |row|
         row.to_hash
-        @order = self.new(order_date: row["Order Date"],
-         customer_id: row["Customer ID"],
-         supplier_id: row["Supplier ID"],
-         delivery_address: row["Delivery Address"],
-         total_order_value_pence: row["Total Order Value"].delete(",").delete(".").to_i,
-         currency: row["Currency"])
+binding.pry
+# creates a new order from hash in case not present already in the database       
+@order = self.new(order_date: row["Order Date"],
+ customer_id: row["Customer ID"],
+ supplier_id: row["Supplier ID"],
+ delivery_address: row["Delivery Address"],
+ total_order_value_pence: row["Total Order Value"].delete(",").delete(".").to_i,
+ currency: row["Currency"])
 
-        @order.save if !self.order_already_exists(@order.order_date, @order.customer_id, @order.supplier_id, @order.delivery_address, @order.currency, @order.total_order_value_pence)
-      end
+@order.save if !self.order_already_exists(@order.order_date, @order.customer_id, @order.supplier_id, @order.delivery_address, @order.currency, @order.total_order_value_pence)
+end
 
-    elsif file.content_type.include? "plain" 
-      lines = []
-      File.open(file.path).each do |line|
+# case 2: importing txt file
+elsif file.content_type.include? "plain"
+
+     # reads the lines in the .txt file and creates an array for each line with the individual order elements (first line as header) 
+     lines = []
+     File.open(file.path).each do |line|
        line = line.strip.split(";")
        line[3].gsub!(/"/, '')
        line[4].gsub!(".", '')
        lines << line
      end
-     rows = {}
-     for i in 1..(lines.length - 1) do
+
+    #creates individual hashes for each order 
+    rows = {}
+    for i in 1..(lines.length - 1) do
       rows[i-1] = {lines[0][0] => lines [i][0], lines[0][1] => lines [i][1], lines[0][2] => lines[i][2], lines[0][3] => lines[i][3], lines[0][4] => lines[i][4], lines[0][5] => lines[i][5]}
     end 
     for i in 0..(rows.length - 1) do
@@ -39,18 +48,19 @@ class Order < ActiveRecord::Base
        rows[i]["Order Date"] = Date.new(date[2].to_i, date[1].to_i, date[0].to_i)
      end
 
-     self.new(order_date: rows[i]["Order Date"],
-       customer_id: rows[i]["Customer ID"],
-       supplier_id: rows[i]["Supplier ID"],
-       delivery_address: rows[i]["Delivery Address"],
-       total_order_value_pence: rows[i]["Total Order Value"].delete(",").delete(".").to_i,
-       currency: rows[i]["Currency"])
+# creates a new order from hash in case not present already in the database
+@order = self.new(order_date: rows[i]["Order Date"],
+ customer_id: rows[i]["Customer ID"],
+ supplier_id: rows[i]["Supplier ID"],
+ delivery_address: rows[i]["Delivery Address"],
+ total_order_value_pence: rows[i]["Total Order Value"].delete(",").delete(".").to_i,
+ currency: rows[i]["Currency"])
 
-     self.save if !order_already_exists(self.order_date, self.customer_id, self.supplier_id, self.delivery_address, self.currency, self.total_order_value_pence)
+@order.save if !self.order_already_exists(@order.order_date, @order.customer_id, @order.supplier_id, @order.delivery_address, @order.currency, @order.total_order_value_pence)
 
-   end
- else
- end
+end
+else
+end
 
 end
 
