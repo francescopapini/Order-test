118629508c1709a150ee190dcabbd070dac94078
francescopapini01@gmail.com <francescopapini01@gmail.com>
2015-06-28 18:59:39 +0100

    adding format for text file

--- ordertest/app/controllers/orders_controller.rb
+++ ordertest/app/controllers/orders_controller.rb
@@ -8,24 +8,27 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find params[:id]
     json_order = { 
-                   order: { id: @order.id, customer: { id: @order.customer_id }},
-                   supplier: { id: @order.supplier_id },
-                   date: @order.order_date.strftime("%d-%m-%Y"),
-                   total_order_value: {local_currency_code: @order.currency, local_value: number_to_currency(@order.total_order_value_pence, unit: ""), value: number_to_currency(@order.convert_order_to_historical_rate(@order.total_order_value_pence, "GBP", @order.order_date), unit: "")}
-                 } 
-    respond_to do |format|
-      format.html 
-      format.json { render json: JSON.pretty_generate(json_order) }
-    end
+     order: { id: @order.id, customer: { id: @order.customer_id }},
+     supplier: { id: @order.supplier_id },
+     date: @order.order_date.strftime("%d-%m-%Y"),
+     total_order_value: {local_currency_code: @order.currency, local_value: number_to_currency(@order.total_order_value_pence, unit: ""), value: number_to_currency(@order.convert_order_to_historical_rate(@order.total_order_value_pence, "GBP", @order.order_date), unit: "")}
+   } 
+   respond_to do |format|
+    format.html 
+    format.json { render json: JSON.pretty_generate(json_order) }
   end
+end
 
-  def new
-  end
+def new
+end
 
-
-  def import
+def import
+  if params[:file]
     Order.import(params[:file])
     redirect_to root_url, notice: "Orders Imported!"
+  else
+    redirect_to new_order_path, notice: "No File To Import, Please Add File"
   end
+end
 
 end
--- ordertest/app/models/order.rb
+++ ordertest/app/models/order.rb
@@ -6,34 +6,58 @@ class Order < ActiveRecord::Base
   validates :total_order_value_pence, numericality: {greater_than_or_equal_to: 0}
 
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-      row.to_hash
-      @order = Order.create(order_date: row["Order Date"],
-       customer_id: row["Customer ID"],
-       supplier_id: row["Supplier ID"],
-       delivery_address: row["Delivery Address"],
-       total_order_value_pence: (row["Total Order Value"].to_f * 100).to_i,
-       currency: row["Currency"])
+    if file.content_type.include? "csv" 
+      CSV.foreach(file.path, headers: true) do |row|
+        row.to_hash
+        Order.create(order_date: row["Order Date"],
+         customer_id: row["Customer ID"],
+         supplier_id: row["Supplier ID"],
+         delivery_address: row["Delivery Address"],
+         total_order_value_pence: (row["Total Order Value"].to_f * 100).to_i,
+         currency: row["Currency"])
+      end
+
+    elsif file.content_type.include? "plain" 
+
+      lines = []
+      File.open(file.path).each do |line|
+
+       line = line.strip.split(";")
+  
+      
+       line[3].gsub!(/"/, '')
+       line[4].gsub!(".", '')
+       lines << line
+     end
+
+     rows = {}
+     for i in 1..(lines.length - 1) do
+      rows[i-1] = {lines[0][0] => lines [i][0], lines[0][1] => lines [i][1], lines[0][2] => lines[i][2], lines[0][3] => lines[i][3], lines[0][4] => lines[i][4], lines[0][5] => lines[i][5]}
+    end 
+
+    for i in 0..(rows.length - 1) do
+      date = rows[i]["Order Date"].split("/")
+      if rows[-1] = "USD"
+       rows[i]["Order Date"] = Date.new(date[2].to_i, date[0].to_i, date[1].to_i)
+      else
+       rows[i]["Order Date"] = Date.new(date[2].to_i, date[1].to_i, date[0].to_i)
+       end
+
+
+      @order = Order.create(order_date: rows[i]["Order Date"],
+       customer_id: rows[i]["Customer ID"],
+       supplier_id: rows[i]["Supplier ID"],
+       delivery_address: rows[i]["Delivery Address"],
+       total_order_value_pence: (rows[i]["Total Order Value"].to_f * 100).to_i,
+       currency: rows[i]["Currency"])
     end
+      binding.pry
+
+    else
+    end
+
   end
 
-
-  # def self.import(file)
-  # lines = []
-  # File.open(file.path).each do |line|
-  #      line = line.strip.split
-  #      lines << line
-  #    end
-  #    for i in 1..lines.length - 1 do
-  #     Order.create(order_date: lines[i][0],
-  #                  customer_id: lines[i][1],
-  #                  supplier_id: lines[i][2],
-  #                  delivery_address: lines[i][3],
-  #                  total_order_value: lines[i][4],
-  #                  currency: lines[i][5])
-  #   end
-  #   binding.pry
-  # end
 
 # converts an order from default currency (USD) into any rate for the specified date
 def convert_order_to_historical_rate(total_order_value, rate, conversion_date)
