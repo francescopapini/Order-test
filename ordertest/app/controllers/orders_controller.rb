5f07e3f3fb6f8236d479aac0e521578ae84c8f02
francescopapini01@gmail.com <francescopapini01@gmail.com>
2015-06-28 22:30:45 +0100

    final clean up

--- ordertest/app/controllers/orders_controller.rb
+++ ordertest/app/controllers/orders_controller.rb
@@ -8,11 +8,12 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find params[:id]
     json_order = { 
-     order: { id: @order.id, customer: { id: @order.customer_id }},
-     supplier: { id: @order.supplier_id },
-     date: @order.order_date.strftime("%d-%m-%Y"),
-     total_order_value: {local_currency_code: @order.currency, local_value: number_to_currency(@order.total_order_value_pence, unit: ""), value: number_to_currency(@order.convert_order_to_historical_rate(@order.total_order_value_pence, "GBP", @order.order_date), unit: "")}
+      order: { id: @order.id, customer: { id: @order.customer_id }},
+      supplier: { id: @order.supplier_id },
+      date: @order.order_date.strftime("%d-%m-%Y"),
+      total_order_value: {local_currency_code: @order.currency, local_value: number_to_currency(@order.total_order_value_pence, unit: ""), value: number_to_currency(@order.convert_order_to_historical_rate(@order.total_order_value_pence, "GBP", @order.order_date), unit: "")}
     } 
+    
     respond_to do |format|
       format.html 
       format.json { render json: JSON.pretty_generate(json_order) }
@@ -30,5 +31,5 @@ class OrdersController < ApplicationController
       redirect_to new_order_path, notice: "No File To Import, Please Add File"
     end
   end
-  
+
 end
